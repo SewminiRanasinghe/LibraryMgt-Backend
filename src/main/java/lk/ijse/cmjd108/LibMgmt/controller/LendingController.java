ca409bdd7b0b73ea259ae3d0d2b11b15380c27da
SewminiRanasinghe <sewmini1010@gmail.com>
2025-05-17 13:18:35 +0530

    Add lending service layer

--- src/main/java/lk/ijse/cmjd108/LibMgmt/controller/LendingController.java
+++ src/main/java/lk/ijse/cmjd108/LibMgmt/controller/LendingController.java
@@ -1,17 +1,25 @@
 package lk.ijse.cmjd108.LibMgmt.controller;
 
 import lk.ijse.cmjd108.LibMgmt.dto.LendingDto;
+import lk.ijse.cmjd108.LibMgmt.service.LendingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/lending")
 public class LendingController {
+    private LendingService lendingService;
+
+    @Autowired
+    public LendingController(LendingService lendingService) {
+        this.lendingService = lendingService;
+    }
+
     @GetMapping("health")
     public String healthTest(){
         return "lending controller is running";
@@ -19,31 +27,30 @@ public class LendingController {
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> addLending(@RequestBody LendingDto lendingDto){
-        System.out.println(lendingDto);
+        lendingService.addLendingData(lendingDto);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @DeleteMapping
     public ResponseEntity<Void> deleteLending(@RequestParam("lendingId") String lendingId){
-        System.out.println(lendingId);
+        lendingService.deleteLendingData(lendingId);
         return ResponseEntity.noContent().build();
     }
 
     @PatchMapping(value = "/{lendingId}",consumes = MediaType.APPLICATION_JSON_VALUE )
     public ResponseEntity<Void> handOverBook(@PathVariable String lendingId, @RequestBody LendingDto lendingDto){
-        System.out.println(lendingId);
-        System.out.println(lendingDto);
+        lendingService.handOverBook(lendingId,lendingDto);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("{lendingId}")
     public ResponseEntity<LendingDto> getSelectedLending(@PathVariable String lendingId){
-        System.out.println("Selected book for "+lendingId);
-        return ResponseEntity.ok(new LendingDto());
+        return ResponseEntity.ok(lendingService.getSelectedLendingData(lendingId));
     }
 
+    @GetMapping
     public ResponseEntity<List<LendingDto>> getAllLendings(){
-        return ResponseEntity.ok(new ArrayList<>());
+        return ResponseEntity.ok(lendingService.getAllLendingData());
     }
 
 }
--- src/main/java/lk/ijse/cmjd108/LibMgmt/dto/LendingDto.java
+++ src/main/java/lk/ijse/cmjd108/LibMgmt/dto/LendingDto.java
@@ -1,20 +1,96 @@
 package lk.ijse.cmjd108.LibMgmt.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
-@AllArgsConstructor
-@NoArgsConstructor
+
 @Data
 public class LendingDto implements Serializable {
     private String lendingId;
     private String book;
     private String member;
+    private String lendingDate;
     private String returnDate;
     private Boolean isActiveLending;
     private Long overDueDays;
     private Double fineAmount;
+
+    public LendingDto(String book, Double fineAmount, Boolean isActiveLending, String lendingDate, String lendingId, String member, Long overDueDays, String returnDate) {
+        this.book = book;
+        this.fineAmount = fineAmount;
+        this.isActiveLending = isActiveLending;
+        this.lendingDate = lendingDate;
+        this.lendingId = lendingId;
+        this.member = member;
+        this.overDueDays = overDueDays;
+        this.returnDate = returnDate;
+    }
+
+    public LendingDto() {
+    }
+
+    public String getBook() {
+        return book;
+    }
+
+    public void setBook(String book) {
+        this.book = book;
+    }
+
+    public Double getFineAmount() {
+        return fineAmount;
+    }
+
+    public void setFineAmount(Double fineAmount) {
+        this.fineAmount = fineAmount;
+    }
+
+    public Boolean getActiveLending() {
+        return isActiveLending;
+    }
+
+    public void setActiveLending(Boolean activeLending) {
+        isActiveLending = activeLending;
+    }
+
+    public String getLendingId() {
+        return lendingId;
+    }
+
+    public void setLendingId(String lendingId) {
+        this.lendingId = lendingId;
+    }
+
+    public String getMember() {
+        return member;
+    }
+
+    public void setMember(String member) {
+        this.member = member;
+    }
+
+    public Long getOverDueDays() {
+        return overDueDays;
+    }
+
+    public void setOverDueDays(Long overDueDays) {
+        this.overDueDays = overDueDays;
+    }
+
+    public String getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(String returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public String getLendingDate() {
+        return lendingDate;
+    }
+
+    public void setLendingDate(String lendingDate) {
+        this.lendingDate = lendingDate;
+    }
 }
