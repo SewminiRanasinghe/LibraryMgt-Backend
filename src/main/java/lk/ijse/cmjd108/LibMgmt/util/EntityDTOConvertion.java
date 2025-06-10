eb25aac6c1bb3104f900abaa6401cb5132991310
SewminiRanasinghe <sewmini1010@gmail.com>
2025-06-10 16:42:50 +0530

    Add mapping for member handling

--- src/main/java/lk/ijse/cmjd108/LibMgmt/util/EntityDTOConvertion.java
+++ src/main/java/lk/ijse/cmjd108/LibMgmt/util/EntityDTOConvertion.java
@@ -1,7 +1,9 @@
 package lk.ijse.cmjd108.LibMgmt.util;
 
 import lk.ijse.cmjd108.LibMgmt.dto.BookDto;
+import lk.ijse.cmjd108.LibMgmt.dto.MemberDto;
 import lk.ijse.cmjd108.LibMgmt.entities.BookEntity;
+import lk.ijse.cmjd108.LibMgmt.entities.MemberEntity;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.stereotype.Component;
@@ -25,5 +27,17 @@ public class EntityDTOConvertion {
     }
     public List<BookDto> toBookDtoList(List<BookEntity> bookEntities){
         return modelMapper.map(bookEntities,new TypeToken<List<BookDto>>(){}.getType());
+    }
+
+    //Member
+    public MemberEntity convertMemberDtoToMemberEntity(MemberDto memberDto){
+        return modelMapper.map(memberDto,MemberEntity.class);
+    }
+    public MemberDto convertMemberEntityTOMemberDTO(MemberEntity memberEntity){
+        return modelMapper.map(memberEntity,MemberDto.class);
+    }
+
+    public List<MemberDto> toMemberDtoList(List<MemberEntity> memberEntities){
+        return modelMapper.map(memberEntities,new TypeToken<List<MemberDto>>(){}.getType());
     }
 }
