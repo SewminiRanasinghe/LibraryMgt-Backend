05028f5849b6c08bc999d4a7064b54b0490f7b12
SewminiRanasinghe <sewmini1010@gmail.com>
2025-06-13 21:44:14 +0530

    Add exception class to handle MemberNotFoundException

---
+++ src/main/java/lk/ijse/cmjd108/LibMgmt/exception/MemberNotFoundException.java
@@ -0,0 +1,13 @@
+package lk.ijse.cmjd108.LibMgmt.exception;
+
+public class MemberNotFoundException extends RuntimeException{
+    public MemberNotFoundException() {
+
+    }
+    public MemberNotFoundException(String message) {
+
+    }
+    public MemberNotFoundException(String message, Throwable cause) {
+
+    }
+}
